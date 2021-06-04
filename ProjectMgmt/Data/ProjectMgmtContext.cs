f517b2e0df3b9b09fc846b1e560f7017cb5fc548
Sarah George <sarahgeorge1394@gmail.com>
2021-06-04 13:19:10 -0400

    Add project files.

---
+++ ProjectMgmt/Data/ProjectMgmtContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMgmt.Models;
+
+namespace ProjectMgmt.Data
+{
+    public class ProjectMgmtContext : DbContext
+    {
+        public ProjectMgmtContext (DbContextOptions<ProjectMgmtContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMgmt.Models.Developer> Developers { get; set; }
+    }
+}
