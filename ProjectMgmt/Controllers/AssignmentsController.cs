26cf2c6eb4d9715709fe90abc9dc4d4454fc253d
Sarah George <sarahgeorge1394@gmail.com>
2021-06-07 09:19:20 -0400

    added migrations and updated the data base

---
+++ ProjectMgmt/Controllers/AssignmentsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectMgmt.Data;
+using ProjectMgmt.Models;
+
+namespace ProjectMgmt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentsController : ControllerBase
+    {
+        private readonly ProjectMgmtContext _context;
+
+        public AssignmentsController(ProjectMgmtContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Assignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Assignment>>> GetAssignments()
+        {
+            return await _context.Assignments.ToListAsync();
+        }
+
+        // GET: api/Assignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Assignment>> GetAssignment(int id)
+        {
+            var assignment = await _context.Assignments.FindAsync(id);
+
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return assignment;
+        }
+
+        // PUT: api/Assignments/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAssignment(int id, Assignment assignment)
+        {
+            if (id != assignment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(assignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Assignments
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Assignment>> PostAssignment(Assignment assignment)
+        {
+            _context.Assignments.Add(assignment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAssignment", new { id = assignment.Id }, assignment);
+        }
+
+        // DELETE: api/Assignments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Assignment>> DeleteAssignment(int id)
+        {
+            var assignment = await _context.Assignments.FindAsync(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Assignments.Remove(assignment);
+            await _context.SaveChangesAsync();
+
+            return assignment;
+        }
+
+        private bool AssignmentExists(int id)
+        {
+            return _context.Assignments.Any(e => e.Id == id);
+        }
+    }
+}
